8685d170bada212ff6614899219924d6d06031df
Anmol saluja <65105427+asaluja00@users.noreply.github.com>
2024-06-24 16:37:21 +0530

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ const App = () => {
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         <Searchbar />
 
+
+
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
